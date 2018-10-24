a0f2ee6f7d6dbf05cebc72af0d8ba3190fc7e9e3
JBallin <JBallin@users.noreply.github.com>
2018-10-24 16:08:58 -0700

    Add personal messages

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Sup World!</h1>
+    <p>I'm a full-stack JavaScript developer who recently graduated from Galvanize's Web Development program.</p>
+    <p>I like to contribute to <a href="https://github.com/JBallin">open source</a> and write technical articles on <a href="https://medium.com/@printsupworld">Medium</a>. <strong>Have a lovely day</strong>!</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Don't click me!!!</Link>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,9 +5,9 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Hey again...I don't have much more to say</h1>
+    <p>If you're bored, go give me some claps on <a href="https://medium.com/@printsupworld">Medium</a>? You're awesome, cheers!</p>
+    <Link to="/">go big or go homie</Link>
   </Layout>
 )
 
